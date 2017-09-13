8beeaf81a0fb6fd1e2323fc29ba9b61dc6c7cea4
sasi <sasipa.wk@gmail.com>
2017-09-13 15:45:52 +0700

    check only zip file and structure of file

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     multer = require('multer'),
-    app = express()
+    pg = require('pg'),
+    dateTime = require('node-datetime'),
+    dt = dateTime.create(),
+    app = express();
 
-var fileuploader = require('./controller/fileuploader') 
+var fileuploader = require('./controller/fileuploader'),
 
-    
+port = 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(function (req, res, next) { //allow cross origin requests
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
@@ -20,10 +25,9 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
+/**call fileuploader */
 app.use(fileuploader)
 
-
-
-app.listen(3000, function () {
-    console.log('App start at localhost 3000......')
+app.listen(port, function () {
+    console.log('App start at localhost' +port+'......')
 })
--- controller/fileuploader.js
+++ controller/fileuploader.js
@@ -1,19 +1,19 @@
-var multer = require('multer')
-var express = require('express')
-var fileuploader = express()
-var fileReader = require('./fileReader')
-
-
-
+var multer = require('multer'),
+    express = require('express'),
+    fileuploader = express(),
+    fileReader = require('./fileReader'),
+    uploaded = require('./uploaded');
+var datetimestamp = Date.now();
+var date = new Date();
+var typeoffile = '';
 /**set folder for file upload*/
 var storage = multer.diskStorage({ //multers disk storage settings
     destination: function (req, file, cb) {
         cb(null, './uploads/');
     },
     filename: function (req, file, cb) {
-        var datetimestamp = Date.now();
-        //cb(null, file.originalname);
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+        typeoffile = '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]
+        cb(null, file.fieldname + '-' + datetimestamp + typeoffile);
     }
 });
 
@@ -24,24 +24,47 @@ var upload = multer({ //multer settings
 
 /** API path that will upload the files */
 fileuploader.post('/upload', function (req, res) {
-        upload(req, res, function (err) {
-            console.log(req.file);
-            if (err) {
-                res.json({ error_code: 1, err_desc: err });
-                return;
-            }
+    upload(req, res, function (err) {
+        console.log(req.file);
+        if (err) {
+            uploaded.uploadInfo(
+                {
+                    uploader: "sasi",
+                    datetimestamp: date,
+                    filename: req.file.filename,
+                    err_msg: err
+                }
+            )
+            res.json({ error_code: 1, err_desc: err });
+        }
+
+        if (typeoffile !== ".zip") {
+            res.send('only file zip')
+            console.log("only zip file")
+        }
+        else {
+            uploaded.uploadInfo(
+                {
+                    uploader: "sasi",
+                    datetimestamp: date,
+                    filename: req.file.filename,
+                    err_msg: "uploaded"
+                }
+            )
+
             fileReader.fileReader(
                 {
-                inputPath: 'uploads/' + req.file.filename,
-                callback: function (list) {
-                    console.log(list)
-                    // for (let i in list) {
-                    //     //วนลูปส่งค่าไปยัง PHIE
-                    // }
-                }
-            })
-           res.json({ status: 'upload success', filename: req.file.filename})
-        }) 
+                    inputPath: 'uploads/' + req.file.filename,
+                    callback: function (list) {
+                        //console.log('list complete')
+                        // for (let i in list) {
+                        //     //วนลูปส่งค่าไปยัง PHIE
+                        // }
+                    }
+                })
+            res.json({ status: 'upload success', filename: req.file.filename })
+        }
+    })
 })
 
 module.exports = fileuploader;
